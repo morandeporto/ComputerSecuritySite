138043264635dcd6397e7df9f064aacb5d00c24e
morandeporto <115929478+morandeporto@users.noreply.github.com>
2024-05-19 10:32:31 +0200

    example and explanation in README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # final_course_project
  
+- DB - Using sqlite3 
+- Web page - using Flask (backend)
+             using html+css (frontend)
+
---
+++ main.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+def create_db():
+    con = sqlite3.connect("final_project.db")
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE "User" (
+	"user_name"	TEXT,
+	"password"	TEXT,
+	"email"	TEXT,
+	"internet_package_type"	INTEGER,
+	" publish_sectors"	INTEGER
+);""")
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    create_db()
